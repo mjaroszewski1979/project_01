c87e8a424da58840e61c31a607a0929679991b5a
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2020-11-07 21:36:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template, url_for,request
-import os
 import smtplib
+import imghdr
+from email.message import EmailMessage
+import os
 
+class User:
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+    
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+users = []
+users.append(User(id=1, username='maciej', password='jaroszewski'))
 
 app = Flask(__name__)
 port = int(os.environ.get('PORT', 5000))
+app.secret_key = 'secretkey'
+
+@app.before_request
+def before_request():
+    if 'user_id' in session:
+        user = [x for x in users if x.id == session['user_id']][0]
+        g.user = user
 
 @app.route('/')
 def index():
@@ -14,21 +33,55 @@ def index():
 def projects():
     return render_template('projects.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
 
 @app.route('/success',methods=['POST'] )
 def success():
     if request.method == "POST":
         name = request.form['name']
         email = request.form['email']
-        message = 'User {name}, {email} have requested contact'.format(email=email, name=name)
-        server = smtplib.SMTP("smtp.gmail.com", 587)
-        server.starttls()
-        server.login('mjaroherokuapp@gmail.com', 'mjaroherokuapp1234')
-        server.sendmail('mjaroszewski1979@gmail.com', email, message)
+        message = 'Thank you {name} for your email. Please enter username: maciej and password: jaroszewski to view my full resume. Go to https://mjaro.herokuapp.com/login'.format(email=email, name=name)
+
+        msg = EmailMessage()
+        msg['Subject'] = 'Confirmation'
+        msg['From'] = 'mjaroherokuapp@gmail.com'
+        msg['To'] = email
+        msg.set_content(message)
+
+        with open('static/images/python.png', 'rb') as f:
+            file_data = f.read()
+            file_type = imghdr.what(f.name)
+            file_name = f.name
+
+        msg.add_attachment(file_data, maintype='image', subtype=file_type, filename=file_name)    
+
+
+        
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            smtp.login('mjaroherokuapp@gmail.com', 'mjaroherokuapp1234')
+            smtp.send_message(msg)
+            
     return render_template('success.html', name=name)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session.pop('user_id', None)
+        username = request.form['username']
+        password = request.form['password']
+
+        user = [x for x in users if x.username == username][0]
+        if user and user.password == password:
+            session['user_id'] = user.id
+            return redirect(url_for('about'))
+        return redirect(url_for('login'))
+    return render_template('login.html')
+
+@app.route('/about')
+def about():
+    if not g.user:
+        return redirect(url_for('login'))
+    return render_template('about.html')
+
 
 if __name__=='__main__':
     port = int(os.environ.get("PORT", 5000))
